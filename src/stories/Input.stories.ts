884e9dd8180568bde542fb2e96084046a98d5f49
bae0203 <gmbae06@dgsw.hs.kr>
2024-10-21 15:27:17 +0900

    fix : storybook Input

--- src/stories/Input.stories.ts
+++ src/stories/Input.stories.ts
@@ -4,16 +4,27 @@ import { fn } from "@storybook/test";
 import Input from "../components/common/Input/Input";
 
 const meta = {
-  title: "IdaTest/Input",
-  component: Input,
-  tags: ["input"],
+  title: "IdaTest/Input", //storybook 내에서의 파일 위치
+  component: Input, //제곧내
+  tags: ["autodocs"],
   parameters: {
-    layout: "test",
+    //storybook에서 보이는거 초기화
+    layout: "centered", //컴포넌트 위치
+    backgrounds: {
+      //배경 색깔 조정
+      values: [
+        { name: "black", value: "#000" },
+        { name: "white", value: "#fff" },
+        { name: "gray", value: "#999" },
+      ],
+    },
   },
   argTypes: {
+    //말 그대로 어떤 타입의 값으로 받는지
     color: { control: "color" },
+    width: { control: { type: "range", min: "100", max: "500", step: "100" } },
   },
-  args: { onChange: fn() },
+  args: { color: "#1485ee", placeholderColor: "#999999", primary: false }, //값 할당
 } satisfies Meta<typeof Input>;
 
 export default meta;
@@ -24,7 +35,6 @@ export const Normal: Story = {
   args: {
     border: 4,
     label: "Input Test",
-    color: "#1485ee",
     placeholder: "test",
   },
 };
@@ -33,7 +43,6 @@ export const Round: Story = {
   args: {
     border: 10,
     label: "Input Test Round",
-    color: "#1485ee",
     placeholder: "round",
   },
 };
@@ -42,7 +51,6 @@ export const Primary: Story = {
   args: {
     ...Normal.args,
     primary: true,
-    color: "#1485ee",
     placeholder: "primary",
     placeholderColor: "#fefefe",
   },
